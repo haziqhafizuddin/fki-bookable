85f4798095cde3209fce0136049064c4fdd22101
haziq <haziqhafizuddintamsir@gmail.com>
2016-04-10 20:19:04 +0800

    added image to equipments

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,4 +1,6 @@
 class Equipment < ActiveRecord::Base
   validates :name, presence: true
   has_many :bookings
+
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160410055614) do
+ActiveRecord::Schema.define(version: 20160410121619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160410055614) do
     t.integer  "quantity"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   create_table "lecturers", force: :cascade do |t|
