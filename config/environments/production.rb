aad75ed2b10894203af2ce329800d7d5b511201d
haziq <haziqhafizuddintamsir@gmail.com>
2016-04-10 14:35:11 +0800

    added sendgrid in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {
+    host: ENV['DEFAULT_URL_OPTIONS'] }
+
+  config.action_mailer.asset_host = ENV['DEFAULT_URL_OPTIONS']
+
+  config.action_mailer.smtp_settings = {
+    address:                'smtp.sendgrid.net',
+    port:                   587,
+    user_name:              ENV['SENDGRID_USERNAME'],
+    password:               ENV['SENDGRID_PASSWORD'],
+    authentication:         'plain',
+    enable_starttls_auto:   true
+  }
 end
